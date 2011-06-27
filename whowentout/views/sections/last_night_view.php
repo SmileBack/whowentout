a906d9dc66c1af49aae21960e9b0f6d2718613f7
danb1 <devnull@localhost>
2011-06-26 17:19:34 -0700

    used codeignitor drop_down syntax

--- whowentout/views/sections/last_night_view.php
+++ whowentout/views/sections/last_night_view.php
@@ -1,13 +1,12 @@
-  <p class="where_did_you_go">Where Did You Go Out Last Night?</p>
-  	
-	<select name="party">	
-		<? foreach ($places as $key => $place): ?>
-		
-			<option><?php print $place->place_name; ?></option>
-		
-		<? endforeach; ?>
-	</select>
-	<input type="submit" />
-	Doors close at 11pm [ in <?php print $timer; ?> ]
+  <p class="where_did_you_go">Where Did You Go Out Last Night?</p> 
+ 	
+<?= form_open('ajax/party_attended', '', array('current_time'=>now())); ?>
+
+<?= form_dropdown('party', $places)?>		
+
+<button type="submit">Submit</button>
 	
+<?= form_close(); ?>
+	
+	Doors close at 11pm [ in <?php print $timer; ?> ]
 		
