109d070bc4a184149a8d2bf4f0437b68f821606a
Venkat Dinavahi <vendiddy@gmail.com>
2011-07-21 14:06:17 -0700

    testing 127.0.0.1

--- whowentout/helpers/job_helper.php
+++ whowentout/helpers/job_helper.php
@@ -53,7 +53,7 @@ function job_run($job_id) {
 }
 
 function job_run_async($job_id) {
-  $job_url = "http://localhost/job/run/$job_id";
+  $job_url = "http://127.0.0.1/job/run/$job_id";
   job_curl_post_async($job_url);
 }
 
