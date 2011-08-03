b39485c8daa9e241ee5e1f049fd3c89e607b368e
Venkat Dinavahi <vendiddy@gmail.com>
2011-08-03 16:45:12 -0700

    Fixed migration error for adding a foreign key.

--- whowentout/migrations/002_Add_party_attendee_fk.php
+++ whowentout/migrations/002_Add_party_attendee_fk.php
@@ -7,7 +7,7 @@ class Migration_Add_party_attendee_fk extends Migration {
     
     $this->db->trans_start();
     $this->db->query('ALTER TABLE `party_attendees`  ADD CONSTRAINT `party_attendee_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)');
-    $this->db->query('ALTER TABLE `party_attendees`  ADD CONSTRAINT `party_attendee_party` FOREIGN KEY (`user_id`) REFERENCES `parties` (`id`)');
+    $this->db->query('ALTER TABLE `party_attendees`  ADD CONSTRAINT `party_attendee_party` FOREIGN KEY (`party_id`) REFERENCES `parties` (`id`)');
     $this->db->trans_complete();
   }
 
