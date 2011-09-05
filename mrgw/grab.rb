1b7fa35cb56da5e241959d4a31abcdac97a049b2
Venkat Dinavahi <vendiddy@gmail.com>
2011-09-04 23:21:41 -0700

    Added common name heuristic to mrgw.

--- mrgw/grab.rb
+++ mrgw/grab.rb
@@ -6,21 +6,34 @@ require './lib/gwudirectoryimporter'
 
 importer = GWUDirectoryImporter.new
 
-def complete_queries
-  Query.with_pattern([4, 1]).complete
-end
-  
-def sorted_incomplete_queries
-  Query.with_pattern([4, 1]).incomplete.order_by_missing
+def incomplete_first_names
+  first_name_tallies = Student.first_name_tallies
+  first_names = []
+  Query.with_pattern(4).incomplete.each do |q|
+    first_names += first_name_tallies.select { |k, v| k.downcase.include?(q.value) }.keys
+  end
+  first_names.uniq.sort
 end
 
-sleep(2)
-sorted_incomplete_queries.each do |query|
-  puts "Starting on #{query.value}..."
-  ('a'..'z').each do |char|
-    pattern = query.value + '+' + char
-    importer.save_students(pattern)
+def incomplete_last_names
+  last_name_tallies = Student.last_name_tallies
+  last_names = []
+  Query.with_pattern(4).incomplete.each do |q|
+    last_names += last_name_tallies.select { |k, v| k.downcase.include?(q.value) }.keys
+  end
+  last_names.uniq.sort
+end
+
+Query.where(:qtype => 'first_name').prioritize_by_missing.each do |q|
+  ('aa'..'zz').each do |suffix|
+    pattern = q.value + '+' + suffix
+    importer.save_students(pattern, 'first_name_combos')
   end
 end
 
-puts "Finished. Great success."
+Query.where(:qtype => 'last_name').prioritize_by_missing.each do |q|
+  ('aa'..'zz').each do |suffix|
+    pattern = q.value + '+' + suffix
+    importer.save_students(pattern, 'last_name_combos')
+  end
+end
--- mrgw/models/query.rb
+++ mrgw/models/query.rb
@@ -5,14 +5,19 @@ class Query < ActiveRecord::Base
   
   has_many :created, :class_name => 'Student', :foreign_key => 'created_by_id'
   
-  attr_accessible :value, :num_total_results, :num_returned_results, :num_in_db, :num_added_to_db
+  attr_accessible :value, :qtype, :num_total_results, :num_returned_results, :num_in_db, :num_added_to_db
   
   validates :value, :presence => true, :uniqueness => true
   
   def has_all_students?
     num_in_db == num_total_results
   end
-  
+
+  def recount_num_in_db
+    self.num_in_db = Student.gwu_query(value).length
+    save
+  end
+
   def self.queried_before(query)
     where(arel_table[:id].lt(query.id))
   end
@@ -26,21 +31,30 @@ class Query < ActiveRecord::Base
   end
   
   def self.complete_condition
-    arel_table[:num_in_db].eq(arel_table[:num_total_results])
+    arel_table[:num_in_db].gteq(arel_table[:num_total_results]) \
+      .and( arel_table[:num_total_results].gteq(0) )
   end
   
-  def self.order_by_missing
+  def self.prioritize_by_missing
     order('queries.num_total_results - queries.num_in_db DESC')
   end
   
-  def self.with_pattern(values)
-    regex = search_pattern_regex(values)
-    puts "re = #{regex}"
-    where('value REGEXP ?', regex)
+  def self.with_pattern(*values)
+    where(with_pattern_condition values)
   end
-  
-  def self.search_pattern_regex(values)
-    return '^' + values.map { |v| '[a-z]{' + v.to_s + '}' }.join('\\+') + '$'
+
+  def self.with_pattern_condition(*values)
+    values.flatten!
+    like = values.map { |c| '_' * c }.join('+')
+    conditions = arel_table[:value].matches(like)
+
+    (0..like.length-1).each do |c|
+      next if like[c] == '+'
+      nl = '_' * like.length
+      nl[c] = '+'
+      conditions = conditions.and(arel_table[:value].matches(nl).not)
+    end
+    return conditions
   end
-  
+
 end
