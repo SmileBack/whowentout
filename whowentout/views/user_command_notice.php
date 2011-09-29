f778fbe035a1f04e8ed97f8fd81f54aaecba3400
Venkat Dinavahi <vendiddy@gmail.com>
2011-09-29 17:29:15 -0400

    Removed see below link and made image width 90px in friends gallery.

--- whowentout/views/user_command_notice.php
+++ whowentout/views/user_command_notice.php
@@ -5,8 +5,7 @@
     <?php elseif ($user->has_checked_in()): ?>
         <?php $party = current_user()->get_checked_in_party(); ?>
         You have checked into <?= $party->place->name ?>.
-        <?= anchor("party/$party->id", 'See below', array('class' => 'show_spotlight', 'data-target' => '#party_summary_' . $party->id, 'data-delay' => 1000)) ?>
-        to go to the party gallery!
+        See below to go to the party gallery!
     <?php else: ?>
         <?php $party_day = $college->party_day(-1, TRUE); ?>
         Check in to the party you attended last night (<?= $party_day->format('l') ?>)!
