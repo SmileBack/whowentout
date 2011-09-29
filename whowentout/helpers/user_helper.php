dccaa8cbb78875c7dff785934f63a9ebbb6b2149
Venkat Dinavahi <vendiddy@gmail.com>
2011-09-28 22:19:26 -0400

    Fixed delete party invitations.

--- whowentout/helpers/user_helper.php
+++ whowentout/helpers/user_helper.php
@@ -66,7 +66,7 @@ function destroy_user($user)
     $ci->db->delete('smiles', array('sender_id' => $user->id));
     $ci->db->delete('smiles', array('receiver_id' => $user->id));
 
-    $ci->db->delete('party_invitations', array('sender_id', $user->id));
+    $ci->db->delete('party_invitations', array('sender_id' => $user->id));
 
     $user->delete();
 }
