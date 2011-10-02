e147b61d181e101fa7be9d22a2e802eff088ddb1
Venkat Dinavahi <vendiddy@gmail.com>
2011-10-02 01:22:03 -0400

    attempt at fixing migration for creating flags table.

--- whowentout/migrations/024_Create_flags_table.php
+++ whowentout/migrations/024_Create_flags_table.php
@@ -6,11 +6,11 @@ class Migration_Create_flags_table extends Migration
     function up()
     {
         $this->db->query("CREATE TABLE `flags` (
-                          `id` varchar(512) NOT NULL,
-                          PRIMARY KEY (`id`)
-                        ) ENGINE=InnoDB");
+                              `id` varchar(512) NOT NULL,
+                              PRIMARY KEY (`id`)
+                            ) ENGINE=InnoDB DEFAULT CHARSET=latin1");
     }
-
+    
     function down()
     {
         $this->db->query("DROP TABLE flags");
