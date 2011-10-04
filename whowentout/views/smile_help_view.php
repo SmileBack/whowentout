2c66986ef8e5f9f492c0fccf3d2487e3a885f6ef
Venkat Dinavahi <vendiddy@gmail.com>
2011-10-03 21:20:18 -0400

    Redid smile help.

--- whowentout/views/smile_help_view.php
+++ whowentout/views/smile_help_view.php
@@ -1,11 +1,4 @@
 <div class="smile_help_container help_container">
-    <div id="smile_help_1">
-        <img src="/assets/images/smile_help_1.png?version=4" />
-        <a href="#smile_help_2" class="next_button nav_button">Next</a>
-    </div>
-    <div id="smile_help_2">
-        <img src="/assets/images/smile_help_2.png?version=4" />
-        <a href="#smile_help_1" class="prev_button nav_button">Prev</a>
-    </div>
+    <img src="/assets/images/smile_help_3.png?version=2" />
 </div>
     
