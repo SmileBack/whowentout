5a3564e80e7493a2c54682c610c89bfc9bce5e30
Venkat Dinavahi <vendiddy@gmail.com>
2011-10-12 18:02:23 -0400

    Removed ci() dependency in Facebook library.

--- whowentout/libraries/fb/facebook.php
+++ whowentout/libraries/fb/facebook.php
@@ -23,75 +23,87 @@ require_once "base_facebook.php";
  */
 class Facebook extends BaseFacebook
 {
-  /**
-   * Identical to the parent constructor, except that
-   * we start a PHP session to store the user ID and
-   * access token if during the course of execution
-   * we discover them.
-   *
-   * @param Array $config the application configuration.
-   * @see BaseFacebook::__construct in facebook.php
-   */
-  public function __construct($config) {
-    if (!session_id()) {
-      session_start();
+
+    private $ci;
+
+    /**
+     * Identical to the parent constructor, except that
+     * we start a PHP session to store the user ID and
+     * access token if during the course of execution
+     * we discover them.
+     *
+     * @param Array $config the application configuration.
+     * @see BaseFacebook::__construct in facebook.php
+     */
+    public function __construct($config)
+    {
+        if (!session_id()) {
+            session_start();
+        }
+        parent::__construct($config);
+
+        $this->ci =& get_instance();
     }
-    parent::__construct($config);
-  }
-  
-  public function request($url, $params) {
-    return $this->makeRequest($url, $params);
-  }
-  
-  protected static $kSupportedKeys =
+
+    public function request($url, $params)
+    {
+        return $this->makeRequest($url, $params);
+    }
+
+    protected static $kSupportedKeys =
     array('state', 'code', 'access_token', 'user_id');
 
-  /**
-   * Provides the implementations of the inherited abstract
-   * methods.  The implementation uses PHP sessions to maintain
-   * a store for authorization codes, user ids, CSRF states, and
-   * access tokens.
-   */
-  protected function setPersistentData($key, $value) {
-    if (!in_array($key, self::$kSupportedKeys)) {
-      self::errorLog('Unsupported key passed to setPersistentData.');
-      return;
+    /**
+     * Provides the implementations of the inherited abstract
+     * methods.  The implementation uses PHP sessions to maintain
+     * a store for authorization codes, user ids, CSRF states, and
+     * access tokens.
+     */
+    protected function setPersistentData($key, $value)
+    {
+        if (!in_array($key, self::$kSupportedKeys)) {
+            self::errorLog('Unsupported key passed to setPersistentData.');
+            return;
+        }
+
+        $session_var_name = $this->constructSessionVariableName($key);
+        $this->ci->session->set_userdata($session_var_name, $value);
     }
 
-    $session_var_name = $this->constructSessionVariableName($key);
-    ci()->session->set_userdata($session_var_name, $value);
-  }
+    protected function getPersistentData($key, $default = false)
+    {
+        if (!in_array($key, self::$kSupportedKeys)) {
+            self::errorLog('Unsupported key passed to getPersistentData.');
+            return $default;
+        }
 
-  protected function getPersistentData($key, $default = false) {
-    if (!in_array($key, self::$kSupportedKeys)) {
-      self::errorLog('Unsupported key passed to getPersistentData.');
-      return $default;
+        $session_var_name = $this->constructSessionVariableName($key);
+        return $this->ci->session->userdata($session_var_name) ?
+                $this->ci->session->userdata($session_var_name) : $default;
     }
 
-    $session_var_name = $this->constructSessionVariableName($key);
-    return ci()->session->userdata($session_var_name) ?
-           ci()->session->userdata($session_var_name) : $default;
-  }
+    protected function clearPersistentData($key)
+    {
+        if (!in_array($key, self::$kSupportedKeys)) {
+            self::errorLog('Unsupported key passed to clearPersistentData.');
+            return;
+        }
 
-  protected function clearPersistentData($key) {
-    if (!in_array($key, self::$kSupportedKeys)) {
-      self::errorLog('Unsupported key passed to clearPersistentData.');
-      return;
+        $session_var_name = $this->constructSessionVariableName($key);
+        $this->ci->session->unset_userdata($session_var_name);
     }
 
-    $session_var_name = $this->constructSessionVariableName($key);
-    ci()->session->unset_userdata($session_var_name);
-  }
-
-  protected function clearAllPersistentData() {
-    foreach (self::$kSupportedKeys as $key) {
-      $this->clearPersistentData($key);
+    protected function clearAllPersistentData()
+    {
+        foreach (self::$kSupportedKeys as $key) {
+            $this->clearPersistentData($key);
+        }
     }
-  }
 
-  protected function constructSessionVariableName($key) {
-    return implode('_', array('fb',
-                              $this->getAppId(),
-                              $key));
-  }
+    protected function constructSessionVariableName($key)
+    {
+        return implode('_', array('fb',
+                                 $this->getAppId(),
+                                 $key));
+    }
 }
