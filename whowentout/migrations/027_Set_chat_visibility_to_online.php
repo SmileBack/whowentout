36e570ffc2a258d55185441e241309b5be1fdb3f
Venkat Dinavahi <vendiddy@gmail.com>
2011-10-19 17:46:49 -0400

    fixed migration so default value of visible_to is online. this fixes the bug where newly registered users appear as offline

--- whowentout/migrations/027_Set_chat_visibility_to_online.php
+++ whowentout/migrations/027_Set_chat_visibility_to_online.php
@@ -5,10 +5,12 @@ class Migration_Set_chat_visibility_to_online extends Migration
     function up()
     {
         $this->db->query("UPDATE users SET visible_to = 'online'");
+        $this->db->query("ALTER TABLE `users`  CHANGE COLUMN `visible_to` `visible_to` VARCHAR(255) NOT NULL DEFAULT 'online' AFTER `chatbar_state`");
     }
 
     function down()
     {
         $this->db->query("UPDATE users SET visible_to = 'everyone'");
+        $this->db->query("ALTER TABLE `users`  CHANGE COLUMN `visible_to` `visible_to` VARCHAR(255) NOT NULL DEFAULT 'everyone' AFTER `chatbar_state`");
     }
 }
