2f91fd7fa699a3df723f5d9820e9335a0eba4f1c
Venkat Dinavahi <vendiddy@gmail.com>
2011-10-21 10:57:15 -0400

    added landing page to google analytics

--- www/landing.php
+++ www/landing.php
@@ -23,16 +23,37 @@
     <script type="text/javascript" src="/assets/js/lib/timeinterval.js"></script>
 
     <script type="text/javascript" src="/assets/js/lib/jquery.class.js"></script>
+    <script type="text/javascript" src="/assets/js/whowentout.component.js?version=2"></script>
     <script type="text/javascript" src="/assets/js/whowentout.queue.js?version=2"></script>
     <script type="text/javascript" src="/assets/js/widgets/jquery.countdowntimer.js?version=2"></script>
     <script type="text/javascript" src="landing.js?version=11"></script>
+
+    <meta name="google-site-verification" content="Qx0f2RFdL3wf2NWU3kcxXacFs020qQ5quH9ZCsVnFlM" />
+    <script type="text/javascript">
+
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-26468050-1']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+            var ga = document.createElement('script');
+            ga.type = 'text/javascript';
+            ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(ga, s);
+        })();
+
+    </script>
 </head>
 
 <body>
 
 <img id="landing" src="/landing.png?version=4"/>
 
-<div id="countdown" class="time_counter" data-target="<?= getenv('countdown_target') ? strtotime(getenv('countdown_target')) : '' ?>">
+<div id="countdown" class="time_counter"
+     data-target="<?= getenv('countdown_target') ? strtotime(getenv('countdown_target'))
+             : strtotime('October 23, 2011') ?>">
     <div class="wrap">
         <div class="days counter" data-length="2"></div>
         <h3>days</h3>
