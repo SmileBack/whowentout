6e9e1b09d5cd25d929fb1af750af0bfc14a1aadc
Venkat Dinavahi <vendiddy@gmail.com>
2011-10-24 16:48:54 -0400

    dashboard now uses a queue to insert attendees

--- www/assets/js/pages/dashboard.js
+++ www/assets/js/pages/dashboard.js
@@ -2,19 +2,81 @@
 //= require lib/jquery.entwine.js
 //= require widgets/jquery.notice.js
 //= require whowentout.application.js
+//= require whowentout.queue.js
 
 $.when(app.load()).then(function() {
+
+    function InsertThumbnailTask(options) {
+        var dfd = $.Deferred();
+        
+        var gallery = $(options.gallery);
+        var user_id = options.user_id;
+        
+        var n = gallery.thumbnailCapacity() - 2;
+        var oldPics = gallery.thumbnails().filter(':gt(' + n + ')');
+        $.when(user(user_id)).then(function(u) {
+            var t = gallery.createThumbnail(u);
+            t.bind('imageload', function() {
+                t.css('opacity', 0);
+                t.css('position', 'fixed');
+                gallery.prepend(t);
+
+                var dim = $(this).hiddenDimensions();
+                var originalWidth = dim.innerWidth;
+                var originalMarginLeft = dim.margin.left;
+                var originalMarginRight = dim.margin.right;
+
+                var speed = 300;
+                t.css({position: '', 'margin-left': '-' + originalWidth + 'px', 'margin-right': '0px'});
+
+                if (oldPics.length > 0) {
+                    oldPics.fadeOut(speed, function() {
+                        $(this).remove();
+                        t.animate({
+                            'margin-left': originalMarginLeft + 'px',
+                            'margin-right': originalMarginRight + 'px'
+                        }, speed)
+                        .animate({opacity: 1}, {
+                            duration: speed,
+                            complete: function() {
+                                dfd.resolve();
+                            }
+                        });
+                    });
+                }
+                else {
+                    t.animate({
+                        'margin-left': originalMarginLeft + 'px'
+                    }, speed)
+                    .animate({opacity: 1}, {
+                        duration: speed,
+                        complete: function() {
+                            dfd.resolve();
+                        }
+                    });
+                }
+            });
+        });
+        
+        return dfd.promise();
+    }
 
     $('.recent_attendees.party').entwine({
         onmatch: function() {
             this._super();
             var self = this;
+
+            this.data('queue', new WhoWentOut.Queue());
+
             app.channel('private-party_' + this.partyID()).bind('checkin', function(e) {
-                self.insertThumbnail(e.user.id);
+                self.insertThumbnail(e.user_id);
             });
         },
         onunmatch: function() {
             this._super();
+        },
+        queue: function() {
+            return this.data('queue');
         },
         thumbnailCapacity: function() {
             return parseInt(this.attr('data-thumbnail-capacity'));
@@ -27,43 +89,10 @@ $.when(app.load()).then(function() {
             return this.find('li');
         },
         insertThumbnail: function(user_id) {
-            var self = this;
-            var n = this.thumbnailCapacity() - 2;
-            var oldPics = this.thumbnails().filter(':gt(' + n + ')');
-            $.when(user(user_id)).then(function(u) {
-                var t = self.createThumbnail(u);
-                t.bind('imageload', function() {
-                    t.css('opacity', 0);
-                    t.css('position', 'fixed');
-                    self.prepend(t);
-
-                    var dim = $(this).hiddenDimensions();
-                    var originalWidth = dim.innerWidth;
-                    var originalMarginLeft = dim.margin.left;
-                    var originalMarginRight = dim.margin.right;
-
-                    var speed = 300;
-                    t.css({position: '', 'margin-left': '-' + originalWidth + 'px', 'margin-right': '0px'});
-
-                    if (oldPics.length > 0) {
-                        oldPics.fadeOut(speed, function() {
-                            $(this).remove();
-                            t.animate({
-                                'margin-left': originalMarginLeft + 'px',
-                                'margin-right': originalMarginRight + 'px'
-                            }, speed)
-                            .animate({opacity: 1}, speed);
-                        });
-                    }
-                    else {
-                        t.animate({
-                            'margin-left': originalMarginLeft + 'px'
-                        }, speed)
-                        .animate({opacity: 1}, speed);
-                    }
-                });
+            this.queue().add(InsertThumbnailTask, {
+                gallery: this,
+                user_id: user_id
             });
-
             return this;
         },
         createThumbnail: function(user) {
@@ -88,7 +117,7 @@ $.when(app.load()).then(function() {
             this.removeClass('hover');
         }
     });
-    
+
     $('.party_summary .link_to_party').entwine({
         onmatch: function() {
             this._super();
