47fa81469a11d7679de827dc00ac64921dcf0af2
Venkat Dinavahi <vendiddy@gmail.com>
2011-10-26 23:16:31 -0400

    disabled update facebook friends

--- whowentout/modules/facebook/updatefacebookfriendsplugin.php
+++ whowentout/modules/facebook/updatefacebookfriendsplugin.php
@@ -3,9 +3,11 @@
 class UpdateFacebookFriendsPlugin extends Plugin
 {
 
+    private $enabled = FALSE;
+
     function on_after_controller_request($e)
     {
-        if ( logged_in() && current_user()->friends_need_update() ) {
+        if ($this->enabled && logged_in() && current_user()->friends_need_update() ) {
             job_call_async('update_facebook_friends', current_user()->id);
         }
     }
