9f64cc96bbd0f99d974b58ad2802adfaaa0f8560
Venkat Dinavahi <vendiddy@gmail.com>
2011-10-28 03:22:23 -0400

    landing page fix

--- whowentout/modules/landing/showlandingpage.plugin.php
+++ whowentout/modules/landing/showlandingpage.plugin.php
@@ -5,7 +5,10 @@ class ShowLandingPagePlugin extends Plugin
 
     function on_before_controller_request($e)
     {
-        if ($e->uri == 'landing')
+        if ($e->uri == 'landing' ||)
+            return;
+
+        if (!college())
             return;
         
         $halloween_launch = new HalloweenLaunch(college()->get_clock());
