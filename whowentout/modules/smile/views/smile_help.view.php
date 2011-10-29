399e31be4b079850e058ce44f6e0073920c67a6b
Venkat Dinavahi <vendiddy@gmail.com>
2011-10-29 14:24:26 -0400

    added underline to smile help

--- whowentout/modules/smile/views/smile_help.view.php
+++ whowentout/modules/smile/views/smile_help.view.php
@@ -1,8 +1,8 @@
 <div class="smile_help_container help_container">
     <?php if (current_user()->gender == 'M'): ?>
-        <img src="/assets/images/smile_help_m.png?version=8" />
+        <img src="/assets/images/smile_help_m.png?version=9" />
     <?php else: ?>
-        <img src="/assets/images/smile_help_f.png?version=8" />
+        <img src="/assets/images/smile_help_f.png?version=9" />
     <?php endif; ?>
 </div>
     
