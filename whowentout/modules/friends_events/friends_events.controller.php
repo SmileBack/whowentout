296f1cc42423ded3e29c54ebc43f7a023d446028
Venkat Dinavahi <vendiddy@gmail.com>
2011-10-29 20:23:12 -0400

    get user ids

---
+++ whowentout/modules/friends_events/friends_events.controller.php
@@ -0,0 +1,56 @@
+<?php
+
+class Friends_Events extends MY_Controller
+{
+
+    function run()
+    {
+        $this->require_login();
+
+        if (!current_user()->is_admin())
+            show_error('Only for admin');
+
+        krumo::dump($this->get_website_gwu_network_facebook_ids());
+    }
+
+    private function get_website_facebook_ids()
+    {
+        $rows = $this->db->select('facebook_id')
+                          ->from('users')
+                          ->where('facebook_access_token IS NOT NULL')->get()->result();
+
+        $facebook_ids = array();
+        foreach ($rows as $row) {
+            $facebook_ids[] = $row->facebook_id;
+        }
+        return $facebook_ids;
+    }
+
+    private function get_website_gwu_network_facebook_ids()
+    {
+        $facebook_ids = $this->get_website_facebook_ids();
+
+        $website_gwu_network_facebook_ids = array();
+        foreach ($facebook_ids as $user_facebook_id) {
+            $facebook_friend_ids = $this->get_user_gwu_facebook_friends($user_facebook_id);
+            $website_gwu_network_facebook_ids = array_merge($website_gwu_network_facebook_ids, $facebook_friend_ids);
+        }
+
+        return $website_gwu_network_facebook_ids;
+    }
+
+
+    private function get_user_gwu_facebook_friends($user_facebook_id)
+    {
+        $result = fb()->api(array(
+                                 'method' => 'fql.query',
+                                 'query' => "SELECT uid, name FROM user WHERE uid IN (SELECT uid1 FROM friend WHERE uid2=$user_facebook_id) AND 'GWU' IN affiliations",
+                            ));
+        $facebook_friend_ids = array();
+        foreach ($result as $k => $row) {
+            $facebook_friend_ids[] = $row['uid'];
+        }
+        return $facebook_friend_ids;
+    }
+
+}
