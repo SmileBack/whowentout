123a4289c4ba58c2080a59f47e6046220b7d184c
Venkat Dinavahi <vendiddy@gmail.com>
2011-11-05 14:46:33 -0400

    added how it works

--- whowentout/modules/whowentout.help/help.controller.php
+++ whowentout/modules/whowentout.help/help.controller.php
@@ -19,7 +19,10 @@ class Help extends MY_Controller
     {
         $this->flag->set('user', current_user()->id, 'has_seen_howitworks_help');
         print r('page', array(
-                          'page_content' => r('howitworks_help'),
+                          'page_content' => r('section', array(
+                                                           'title' => 'How it Works',
+                                                           'body' => r('howitworks_help'),
+                                                         )),
                         ));
     }
 
