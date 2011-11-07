92f1aa1b57d8e7dbddd8867e79caa25d0159a20c
Venkat Dinavahi <vendiddy@gmail.com>
2011-11-06 21:36:45 -0500

    re-added come back thursday message

--- whowentout/modules/whowentout.website/views/dashboard.view.php
+++ whowentout/modules/whowentout.website/views/dashboard.view.php
@@ -3,12 +3,10 @@
                    'body' => '',
                  )) ?>
 
-<?php if (FALSE): ?>
 <div class="dashboard_message notice_style" style="margin-top: 20px;">
     <h2>Check-ins have closed for this weekend.</h2>
     <h2>Check-ins will re-open on Thursday for next weekend's parties!</h2>
 </div>
-<?php endif; ?>
 
 <?=
 r('parties_attended', array(
