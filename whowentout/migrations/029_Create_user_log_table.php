903faf46b5d63c26a6ce28899aba9a47a586e1fc
Venkat Dinavahi <vendiddy@gmail.com>
2011-11-09 17:57:41 -0500

    removed user_id foreign key from user_log migration

--- whowentout/migrations/029_Create_user_log_table.php
+++ whowentout/migrations/029_Create_user_log_table.php
@@ -10,9 +10,7 @@ class Migration_Create_user_log_table extends Migration
                               `action` varchar(255) NOT NULL,
                               `time` datetime DEFAULT NULL,
                               `data` text,
-                              PRIMARY KEY (`id`),
-                              KEY `user_log_user_id_fk` (`user_id`),
-                              CONSTRAINT `user_log_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+                              PRIMARY KEY (`id`)
                             ) ENGINE=InnoDB");
     }
 
