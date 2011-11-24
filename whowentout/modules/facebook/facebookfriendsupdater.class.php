8ec95dff1659d29843a569b103103c318fc5744e
Venkat Dinavahi <vendiddy@gmail.com>
2011-11-24 15:31:08 -0500

    final commit for whowentout_gw

--- whowentout/modules/facebook/facebookfriendsupdater.class.php
+++ whowentout/modules/facebook/facebookfriendsupdater.class.php
@@ -1,9 +1,6 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Venkat
- * Date: 11/15/11
- * Time: 6:40 PM
- * To change this template use File | Settings | File Templates.
- */
- 
+
+class FacebookProfileSource
+{
+    
+}
