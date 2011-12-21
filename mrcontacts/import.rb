246727fa4968adf30d69e5085ebf7471bf6c2ba3
Venkat Dinavahi <vendiddy@gmail.com>
2011-12-21 17:41:44 -0500

    different combinations for each college

--- mrcontacts/import.rb
+++ mrcontacts/import.rb
@@ -12,7 +12,7 @@ require './directories/gwu_directory'
 require './directories/georgetown_directory'
 require './directories/cua_directory'
 
-def get_college_directory(college)
+def college_directory(college)
   if college == 'gwu'
     directory = GWUDirectory.new
     directory.login('dberen27', 'Apple12345678!')
@@ -24,19 +24,39 @@ def get_college_directory(college)
   end
 end
 
+def college_combinations(college)
+  if college == 'gwu'
+    return ('aaa'..'zzz')
+  elsif college == 'georgetown'
+    return ('aaa'..'zzz')
+  elsif college == 'cua'
+    return ('aa'..'zz')
+  end
+end
+
+def valid_college?(college)
+  ['gwu', 'georgetown', 'cua'].include?(college)
+end
+
 def begin_import(college)
+  if !valid_college?(college)
+    raise "#{college} is invalid"
+  end
+
   connect_to_database(college)
 
-  directory = get_college_directory(college)
+  directory = college_directory(college)
   directory_importer = DirectoryImporter.new(directory)
 
   directory_logger = DirectoryLogger.new(directory)
   directory_importer_logger = DirectoryImporterLogger.new(directory_importer)
 
-  ('aa'..'zz').each do |combination|
+  college_combinations(college).each do |combination|
     directory_importer.import combination
   end
 end
 
 college = ARGV[0]
 begin_import(college)
+
+
