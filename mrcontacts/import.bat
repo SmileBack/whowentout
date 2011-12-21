5108bd1a4025da70b7aed7f089ac27be29598605
Venkat Dinavahi <vendiddy@gmail.com>
2011-12-21 18:50:52 -0500

    added a batch file that restarts ruby scripts when they crash

---
+++ mrcontacts/import.bat
@@ -0,0 +1,5 @@
+:loop
+ECHO --STARTING SCRIPT--
+ruby import.rb %1
+GOTO LOOP
+
---
+++ whowentout/views/event/event_invite_link.tpl.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Venkat
+ * Date: 12/21/11
+ * Time: 6:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
