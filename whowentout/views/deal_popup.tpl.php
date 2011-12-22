d52cbc43399cb59d0450e323c8a0cb4f671dc33d
Venkat Dinavahi <vendiddy@gmail.com>
2011-12-22 18:09:48 -0500

    added a deal popup

--- whowentout/views/deal_popup.tpl.php
+++ whowentout/views/deal_popup.tpl.php
@@ -1,8 +1,16 @@
-<?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Venkat
- * Date: 12/22/11
- * Time: 1:46 PM
- * To change this template use File | Settings | File Templates.
- */
+<form class="deal_popup" method="post" action="/events/deal_confirm">
+
+    <h2>The deal will be sent to your phone</h2>
+
+    <img alt="sample deal" src="/images/deal_popup.png"/>
+
+    <div>
+        <label>
+            Give us your cell phone number so we can send the deal
+        </label>
+        <input type="text" name="user[cell_phone_number]" value="<?= $user->cell_phone_number ?>"/>
+    </div>
+
+    <input type="submit" value="OK" />
+
+</form>
