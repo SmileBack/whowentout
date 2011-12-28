c2d9fab8c39ac76e40adb6ee9003491b092075c0
Venkat Dinavahi <vendiddy@gmail.com>
2011-12-28 01:51:08 -0500

    update fb friends plugin with skeleton

--- whowentout/plugins/update_facebook_friends.plugin.php
+++ whowentout/plugins/update_facebook_friends.plugin.php
@@ -1,8 +1,12 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Venkat
- * Date: 12/28/11
- * Time: 1:36 AM
- * To change this template use File | Settings | File Templates.
- */
+
+class UpdateFacebookFriendsPlugin extends Plugin
+{
+    function on_boot($e)
+    {
+        // if user is logged in
+        //  if user has not had friends updated in 1 week
+        //  update facebook friends for the user
+        // @todo: last updated column should be updated in the facebook friends updater
+    }
+}
