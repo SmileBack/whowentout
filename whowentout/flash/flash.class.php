bf4d28bae4f7581e39ce0aec4fbbe92c156613ba
Venkat Dinavahi <vendiddy@gmail.com>
2011-12-29 13:24:48 -0500

    fixed a bug where flash messages were empty when you retrieved them

--- whowentout/flash/flash.class.php
+++ whowentout/flash/flash.class.php
@@ -9,7 +9,7 @@ class flash
             $_SESSION['flash_message'] = $message;
         }
         else {
-            $message = isset($_SESSION['flash_message']) ? $message : '';
+            $message = isset($_SESSION['flash_message']) ? $_SESSION['flash_message'] : '';
             unset($_SESSION['flash_message']);
         }
         return r::flash_message(array('message' => $message));
