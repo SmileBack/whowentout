b73a6857bdb2a085e3eaa583896f0491a5251daa
Venkat Dinavahi <vendiddy@gmail.com>
2011-12-29 12:54:31 -0500

    extracted jswindowobject out of jsobject and removed conditionals. jswindowobject now automatically persists to the  variable so the commands are guaranteed to run even if there are redirects

--- whowentout/js_bridge/jsobject.class.php
+++ whowentout/js_bridge/jsobject.class.php
@@ -8,10 +8,10 @@ class JsObject
     public $name;
 
     /* @var $commands JsCommand[] */
-    private $commands = array();
+    protected $commands = array();
 
     /* @var $objects JsObject[] */
-    private $objects = array();
+    protected $objects = array();
 
     function __construct($name)
     {
@@ -46,39 +46,9 @@ class JsObject
         unset($this->objects[$name]);
     }
 
-    function to_js()
-    {
-        $js = array();
-        foreach ($this->commands as $command) {
-            $js[] = $command->to_js() . ';';
-        }
-        return implode("\n", $js);
-    }
-
-    function __toString()
-    {
-        return $this->script();
-    }
-
-    function script()
-    {
-        $script = array(
-            '<script type="text/javascript">',
-            $this->to_js(),
-            '</script>',
-            '',
-        );
-        return implode("\n", $script);
-    }
-
     function _add_command(JsCommand $command)
     {
-        if ($this->parent == null) {
-            $this->commands[] = $command;
-        }
-        else {
-            $this->parent->_add_command($command);
-        }
+        $this->parent->_add_command($command);
     }
 
     function _get_path()
---
+++ whowentout/js_bridge/jswindowobject.class.php
@@ -0,0 +1,69 @@
+<?php
+
+class JsWindowObject extends JsObject
+{
+
+    function __construct()
+    {
+        parent::__construct('window');
+        $this->load_from_session();
+    }
+
+    function _add_command(JsCommand $command)
+    {
+        $this->commands[] = $command;
+    }
+
+    function __toString()
+    {
+        $script = $this->script();
+
+        // clear everything so the script is only run once
+        $this->commands = array();
+        $this->objects = array();
+
+        return $script;
+    }
+
+    function script()
+    {
+        $script = array(
+            '<script type="text/javascript">',
+            $this->to_js(),
+            '</script>',
+            '',
+        );
+        return implode("\n", $script);
+    }
+
+    function to_js()
+    {
+        $js = array();
+        foreach ($this->commands as $command) {
+            $js[] = $command->to_js() . ';';
+        }
+        return implode("\n", $js);
+    }
+
+    function __destruct()
+    {
+        $this->persist_to_session();
+    }
+
+    private function load_from_session()
+    {
+        if (isset($_SESSION['js_window_object'])) {
+            $this->commands = $_SESSION['js_window_object']['commands'];
+            $this->objects = $_SESSION['js_window_object']['objects'];
+        }
+    }
+
+    private function persist_to_session()
+    {
+        $_SESSION['js_window_object'] = array(
+            'commands' => $this->commands,
+            'objects' => $this->objects,
+        );
+    }
+
+}
