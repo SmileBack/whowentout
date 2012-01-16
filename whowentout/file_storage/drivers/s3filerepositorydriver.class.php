37c290b6d828da6b2fbba3f96fc8a297ceaa2f14
Venkat Dinavahi <vendiddy@gmail.com>
2012-01-16 17:26:56 -0500

    updated s3driver to use latest s3 lib

--- whowentout/file_storage/drivers/s3filerepositorydriver.class.php
+++ whowentout/file_storage/drivers/s3filerepositorydriver.class.php
@@ -43,10 +43,51 @@ class S3FileRepositoryDriver extends FileRepositoryDriver
     private function s3()
     {
         if ($this->s3 == NULL) {
-            $this->s3 = new AmazonS3($this->options['amazon_public_key'], $this->options['amazon_secret_key']);
+            $this->set_s3_credentials();
+            $this->s3 = new AmazonS3();
             $this->s3()->use_ssl = false;
         }
         return $this->s3;
+    }
+
+    private function set_s3_credentials()
+    {
+        /**
+         * Create a list of credential sets that can be used with the SDK.
+         */
+        CFCredentials::set(array(
+        	// Credentials for the development environment.
+        	'development' => array(
+
+        		// Amazon Web Services Key. Found in the AWS Security Credentials. You can also pass
+        		// this value as the first parameter to a service constructor.
+        		'key' => $this->options['amazon_public_key'],
+
+        		// Amazon Web Services Secret Key. Found in the AWS Security Credentials. You can also
+        		// pass this value as the second parameter to a service constructor.
+        		'secret' => $this->options['amazon_secret_key'],
+
+        		// This option allows you to configure a preferred storage type to use for caching by
+        		// default. This can be changed later using the set_cache_config() method.
+        		//
+        		// Valid values are: `apc`, `xcache`, or a file system path such as `./cache` or
+        		// `/tmp/cache/`.
+        		'default_cache_config' => '',
+
+        		// Determines which Cerificate Authority file to use.
+        		//
+        		// A value of boolean `false` will use the Certificate Authority file available on the
+        		// system. A value of boolean `true` will use the Certificate Authority provided by the
+        		// SDK. Passing a file system path to a Certificate Authority file (chmodded to `0755`)
+        		// will use that.
+        		//
+        		// Leave this set to `false` if you're not sure.
+        		'certificate_authority' => false
+        	),
+
+        	// Specify a default credential set to use if there are more than one.
+        	'@default' => 'development'
+        ));
     }
 
 }
