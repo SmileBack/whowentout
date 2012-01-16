ba2242f1d0937eb2d4e77a8f781df0a9a087e0dd
Venkat Dinavahi <vendiddy@gmail.com>
2012-01-16 14:03:51 -0500

    debug 8

--- www/index.php
+++ www/index.php
@@ -12,7 +12,9 @@ print '<br/>';
 require_once FIREPATH . 'debug/krumo.class.php';
 require_once FIREPATH . 'core/core.functions.php';
 
+print "<h1>before env</h1>";
 print "<h1> env = " . environment() . "</h1>";
+print "<h1>after env</h1>";
 
 print "<h1>got here (require core woo yea) </h1>";
 
