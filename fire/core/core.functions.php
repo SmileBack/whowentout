fd588e0d8e4d9f8d33eacb58a63180644465768a
Venkat Dinavahi <vendiddy@gmail.com>
2012-01-16 17:16:00 -0500

    added version compare

--- fire/core/core.functions.php
+++ fire/core/core.functions.php
@@ -53,8 +53,6 @@ function factory($environment = null)
         $index_cache = new FilesystemCache(APPPATH . 'cache');
         $index = new Index(APPPATH, $index_cache);
 
-        krumo::dump($index->data());
-
         $class_loader = new ClassLoader($index);
         $class_loader->enable_autoload();
 
