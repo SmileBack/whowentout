a060936334812c03a32fb7a3341e7373dcfa565c
Venkat Dinavahi <vendiddy@gmail.com>
2012-01-16 15:11:22 -0500

    cleaned index.php

--- www/index.php
+++ www/index.php
@@ -5,25 +5,8 @@ error_reporting(E_ALL);
 define('FIREPATH', dirname(__FILE__) . '/../fire/');
 define('APPPATH', dirname(__FILE__) . '/../');
 
-print "<h1>got here (define paths)</h1>";
-print '<h2>' . FIREPATH . '</h2>';
-print '<br/>';
-print '<h2>' . APPPATH . '</h2>';
-print '<br/>';
-
 require_once FIREPATH . 'debug/krumo.class.php';
 require_once FIREPATH . 'core/core.functions.php';
-
-print "<h1>before env</h1>";
-print "<h1> env = " . environment() . "</h1>";
-print "<h1>after env</h1>";
-
-print "<h1>got here (require core woo yea) </h1>";
-
-
-print "<h1>before db</h1>";
-db();
-print "<h1>after db</h1>";
 
 if (db()->has_table('sessions')) {
     $session_handler = factory()->build('session_handler');
@@ -37,17 +20,8 @@ if (db()->has_table('sessions')) {
     );
 }
 
-print "<h1>got here (before session)</h1>";
-
-print "<h1>environment = " . environment() . "</h1>";
-
 session_start();
 
 app()->clock()->set_time(new DateTime('2011-12-09'));
 
-print "<h1>got here (after set time)</h1>";
-
 route_uri_request();
-
-print "<h1>got here (after routing uri request)</h1>";
-
