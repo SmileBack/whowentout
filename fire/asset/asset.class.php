aa083d6c2341718a9bc61192e3abcb0358dafd79
Venkat Dinavahi <vendiddy@gmail.com>
2012-01-17 15:09:10 -0500

    asset class now takes timestamps into account

--- fire/asset/asset.class.php
+++ fire/asset/asset.class.php
@@ -26,11 +26,51 @@ class Asset
         $this->js[] = $js_name;
     }
 
+    function scripts()
+    {
+        $scripts = array();
+        foreach ($this->get_loaded_js() as $js) {
+            $scripts[] = $this->script($js);
+        }
+        return implode("\n", $scripts);
+    }
+
     function get_loaded_js()
     {
         $tree = $this->get_dependency_tree();
         $ts = new TopologicalSort($tree);
         return $ts->dependencies_of($this->js);
+    }
+
+    private function script($name)
+    {
+        $tpl = '<script type="text/javascript" src="%s"></script>';
+        $path = $this->get_file_path($name);
+        $path = string_before_last('.js', $path) . '.' . $this->get_modification_time($name) . '.js';
+        return sprintf($tpl, $path);
+    }
+
+    private function get_file_path($js_name)
+    {
+        /* @var $js_meta JsMetadata */
+        $js_meta = $this->index->get_metadata($js_name);
+
+        $filepath = $js_meta->filepath;
+        $filepath = str_replace('\\', '/', $filepath);
+
+        $root_dir = dirname($_SERVER['SCRIPT_FILENAME']);
+
+        if (string_starts_with($root_dir, $filepath))
+            $filepath = string_after_first($root_dir, $filepath);
+
+        return $filepath;
+    }
+
+    private function get_modification_time($js_name)
+    {
+        /* @var $js_meta JsMetadata */
+        $js_meta = $this->index->get_metadata($js_name);
+        return filemtime($js_meta->filepath);
     }
 
     function get_dependency_tree()
