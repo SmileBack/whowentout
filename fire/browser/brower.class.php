ed32435b703bd81307f15db819cbcdd5cba0f70c
Venkat Dinavahi <vendiddy@gmail.com>
2012-01-18 14:52:59 -0500

    very basic browser detection code

--- fire/browser/brower.class.php
+++ fire/browser/brower.class.php
@@ -1,1 +1,17 @@
 <?php
+
+class browser
+{
+
+    static function is_mobile()
+    {
+        if(preg_match('/(alcatel|amoi|android|avantgo|blackberry|benq|cell|cricket|docomo|elaine|htc|iemobile|iphone|ipad|ipaq|ipod|j2me|java|midp|mini|mmp|mobi|motorola|nec-|nokia|palm|panasonic|philips|phone|sagem|sharp|sie-|smartphone|sony|symbian|t-mobile|telus|up\.browser|up\.link|vodafone|wap|webos|wireless|xda|xoom|zte)/i', $_SERVER['HTTP_USER_AGENT']))
+            return true;
+
+        else
+            return false;
+    }
+
+}
+
+
--- www/test.php
+++ www/test.php
@@ -1,2 +1,16 @@
-<h1>here is a test</h1>
-<?= date('Y-m-d H:i:s'); ?>
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+require_once '../fire/core/boot.php';
+boot();
+
+?>
+
+<?php if (browser::is_mobile()): ?>
+        <h1>you are on a mobile browser</h1>
+<?php else: ?>
+        <h1>you are <em>not</em> on a mobile browser</h1>
+<?php endif; ?>
+
