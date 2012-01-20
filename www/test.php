7ac1921f4037148d38a902e75bf50e502afa98c1
Venkat Dinavahi <vendiddy@gmail.com>
2012-01-20 18:16:02 -0500

    test commit 1

--- www/test.php
+++ www/test.php
@@ -3,11 +3,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-require_once '../fire/core/boot.php';
-boot();
-
-/* @var $package_installer PackageInstaller */
-$package_installer = build('package_installer');
-$package_installer->update('WhoWentOutPackage');
-print "Updated package";
+$contents = file_get_contents('./../whowentout/config/app.yml');
 ?>
+<pre><?= $contents ?></pre>
+
