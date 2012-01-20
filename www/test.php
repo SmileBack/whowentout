e8fc179aecf0fc99d9752846ba0619af1a0eaf46
Venkat Dinavahi <vendiddy@gmail.com>
2012-01-20 11:56:03 -0500

    whowentout package update

--- www/test.php
+++ www/test.php
@@ -6,17 +6,8 @@ ini_set('display_errors', '1');
 require_once '../fire/core/boot.php';
 boot();
 
-$action_router = new ActionRouter();
-$action_router->add('date/(:any)', 'ViewDayAction/$1');
-
-//$action_router->execute('date/20120119');
-
-$parser = new PHPClassParser();
-$classes = $parser->get_file_classes(APPPATH . 'whowentout/controllers/woo.php');
-krumo::dump($classes);
-
-//$target = $route_matcher->route('date/20120119');
-//krumo::dump($target);
-//$action = new ViewDayAction();
-//$action->execute('20120119');
+/* @var $package_installer PackageInstaller */
+$package_installer = build('package_installer');
+$package_installer->update('WhoWentOutPackage');
+print "Updated package";
 ?>
