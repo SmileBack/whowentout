4a5457828013b37b3a2ada8ba5807308275b7fb2
Venkat Dinavahi <vendiddy@gmail.com>
2012-01-20 12:17:29 -0500

    made width 100%

--- whowentout/views/page.tpl.php
+++ whowentout/views/page.tpl.php
@@ -19,7 +19,10 @@
 
     <link rel="stylesheet/less" type="text/css" media="only screen and (max-device-width: 480px)"
           href="/css/styles.mobile.<?= filemtime('./css/styles.mobile.less') ?>.less"/>
-    
+
+    <!--<link rel="stylesheet/less" type="text/css" media="screen and (max-width: 800px)"
+          href="/css/styles.mobile.<?= filemtime('./css/styles.mobile.less') ?>.less"/>-->
+
     <script src="/js/less.js" type="text/javascript"></script>
 
     <?php
