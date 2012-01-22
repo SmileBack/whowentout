b9b420cfbe262358349a58bf2e202e9c21089d39
Venkat Dinavahi <vendiddy@gmail.com>
2012-01-22 13:33:36 -0500

    removed debug statements from test.php

--- www/test.php
+++ www/test.php
@@ -5,16 +5,4 @@ ini_set('display_errors', '1');
 
 require_once '../fire/core/boot.php';
 boot();
-
-$filepath = APPPATH . 'whowentout/checkin/tests/checkin_engine_tests.yml';
-krumo::dump(file_exists($filepath));
-krumo::dump(realpath($filepath));
-?>
-
-<pre>
-    <?= file_get_contents($filepath) ?>;
-</pre>
-
-<?php
-unlink(APPPATH . 'whowentout/checkin/tests/checkin_engine_tests.yml');
 ?>
