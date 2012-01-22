316666f493151de818c880d6a90c69d597337fc9
Venkat Dinavahi <vendiddy@gmail.com>
2012-01-22 12:45:14 -0500

    print yml contents for checkin engine tests

--- www/test.php
+++ www/test.php
@@ -7,6 +7,7 @@ require_once '../fire/core/boot.php';
 boot();
 
 krumo::dump(file_exists(APPPATH . 'whowentout/checkin/tests/checkin_engine_tests.yml'));
+krumo::dump(file_get_contents(APPPATH . 'whowentout/checkin/tests/checkin_engine_tests.yml'));
 
 unlink(APPPATH . 'whowentout/checkin/tests/checkin_engine_tests.yml');
 
