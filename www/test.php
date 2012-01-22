2d69866a2bbd491c4501dfa7be642282f7b140f3
Venkat Dinavahi <vendiddy@gmail.com>
2012-01-22 12:26:59 -0500

    debug printing checkin_engine.yml contents

--- www/test.php
+++ www/test.php
@@ -6,6 +6,13 @@ ini_set('display_errors', '1');
 require_once '../fire/core/boot.php';
 boot();
 
-
-
+$contents_yml = file_get_contents('./checkin/tests/checkin_engine_tests.yml');
+$contents_php = file_get_contents('./checkin/tests/checkin_engine_tests.php');
 ?>
+<pre>
+    <?= $contents_yml ?>
+</pre>
+
+<pre>
+    <?= $contents_php ?>
+</pre>
