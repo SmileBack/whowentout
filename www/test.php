ede75b56d5819974ec78d99bd43caa004bb24f89
Venkat Dinavahi <vendiddy@gmail.com>
2012-01-22 12:44:27 -0500

    file exists checkin_engine_tests.yml

--- www/test.php
+++ www/test.php
@@ -6,6 +6,8 @@ ini_set('display_errors', '1');
 require_once '../fire/core/boot.php';
 boot();
 
+krumo::dump(file_exists(APPPATH . 'whowentout/checkin/tests/checkin_engine_tests.yml'));
+
 unlink(APPPATH . 'whowentout/checkin/tests/checkin_engine_tests.yml');
 
 ?>
