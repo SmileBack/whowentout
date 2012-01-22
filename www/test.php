fc8d493cd1515c39b8bec50bcf773f5830b95b88
Venkat Dinavahi <vendiddy@gmail.com>
2012-01-22 12:47:48 -0500

    realpath and contents

--- www/test.php
+++ www/test.php
@@ -7,11 +7,14 @@ require_once '../fire/core/boot.php';
 boot();
 
 $filepath = APPPATH . 'whowentout/checkin/tests/checkin_engine_tests.yml';
-
-krumo::dump(file_exists(APPPATH . 'whowentout/checkin/tests/checkin_engine_tests.yml'));
+krumo::dump(file_exists($filepath));
 krumo::dump(realpath($filepath));
-krumo::dump(file_get_contents(APPPATH . 'whowentout/checkin/tests/checkin_engine_tests.yml'));
+?>
 
+<pre>
+    <?= file_get_contents($filepath) ?>;
+</pre>
+
+<?php
 unlink(APPPATH . 'whowentout/checkin/tests/checkin_engine_tests.yml');
-
 ?>
