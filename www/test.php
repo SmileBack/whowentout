82cca97bc29edd5505a8fa29dfad6cf04b1ec826
Venkat Dinavahi <vendiddy@gmail.com>
2012-01-22 12:46:22 -0500

    realpath

--- www/test.php
+++ www/test.php
@@ -6,7 +6,10 @@ ini_set('display_errors', '1');
 require_once '../fire/core/boot.php';
 boot();
 
+$filepath = APPPATH . 'whowentout/checkin/tests/checkin_engine_tests.yml';
+
 krumo::dump(file_exists(APPPATH . 'whowentout/checkin/tests/checkin_engine_tests.yml'));
+krumo::dump(realpath($filepath));
 krumo::dump(file_get_contents(APPPATH . 'whowentout/checkin/tests/checkin_engine_tests.yml'));
 
 unlink(APPPATH . 'whowentout/checkin/tests/checkin_engine_tests.yml');
