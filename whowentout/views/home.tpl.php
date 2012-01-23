23120b8d25c74c0dc66f1aabd43da2cdc0b177df
Venkat Dinavahi <vendiddy@gmail.com>
2012-01-23 11:30:39 -0500

    made homepage scale to width of screen

--- whowentout/views/home.tpl.php
+++ whowentout/views/home.tpl.php
@@ -17,6 +17,12 @@
     <link rel="stylesheet/less" type="text/css" href="/css/jquery.jcrop.less"/>
     <link rel="stylesheet/less" type="text/css" href="/css/styles.<?= time() ?>.less"/>
 
+    <link rel="stylesheet/less" type="text/css" media="only screen and (max-device-width: 480px)"
+          href="/css/styles.mobile.<?= filemtime('./css/styles.mobile.less') ?>.less"/>
+
+    <!--<link rel="stylesheet/less" type="text/css" media="screen and (max-width: 800px)"
+          href="/css/styles.mobile.<?= filemtime('./css/styles.mobile.less') ?>.less"/>-->
+
     <script src="/js/less.js" type="text/javascript"></script>
     <script src="/js/head.load.min.js" type="text/javascript"></script>
 
