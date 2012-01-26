bc268c06ddb78d44e5d144c123cc3ba15ea1a8d1
Venkat Dinavahi <vendiddy@gmail.com>
2012-01-26 13:59:15 -0500

    yet another fucking attempt for hiding the addr bar

--- www/js/pages/mobile_deal_page.js
+++ www/js/pages/mobile_deal_page.js
@@ -45,3 +45,17 @@ $('.ticket').entwine({
     }
 });
 
+function hideAddressBar()
+{
+  if(!window.location.hash)
+  {
+      if(document.height < window.outerHeight)
+      {
+          document.body.style.height = (window.outerHeight + 50) + 'px';
+      }
+
+      setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
+  }
+}
+window.addEventListener("load", function(){ if(!window.pageYOffset){ hideAddressBar(); } } );
+window.addEventListener("orientationchange", hideAddressBar );
