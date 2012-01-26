77bbb8b541127642d6077c18762abba09d5c7187
Venkat Dinavahi <vendiddy@gmail.com>
2012-01-26 13:50:25 -0500

    remove address bar on deal page

--- www/js/pages/mobile_deal_page.js
+++ www/js/pages/mobile_deal_page.js
@@ -44,3 +44,23 @@ $('.ticket').entwine({
         });
     }
 });
+
+
+function hideAddressBar()
+{
+  if(!window.location.hash)
+  {
+      if(document.height < window.outerHeight)
+      {
+          document.body.style.height = (window.outerHeight + 50) + 'px';
+      }
+
+      setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
+  }
+}
+$(window).bind('load', function() {
+    if(!window.pageYOffset)
+        hideAddressBar();
+});
+$(window).bind('orientationchange', hideAddressBar);
+window.addEventListener("orientationchange", hideAddressBar);
