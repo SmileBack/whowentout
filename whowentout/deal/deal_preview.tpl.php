d74e6fd111b6ab4b37e895ee80e0180da430c2b7
Venkat Dinavahi <vendiddy@gmail.com>
2012-01-25 19:49:35 -0500

    made deal dialog sweet

--- whowentout/deal/deal_preview.tpl.php
+++ whowentout/deal/deal_preview.tpl.php
@@ -2,6 +2,6 @@
     <img class="phone" alt="sample deal" src="/images/iphone_blank.png"/>
     <img class="ticket" src="<?= $ticket_url ?>"/>
     <?php if (!browser::is_mobile()): ?>
-    <?= a('profile/picture/edit', 'change', array('class' => 'edit_pic_link')) ?>
+    <?= a('profile/picture/edit', 'change pic', array('class' => 'edit_pic_link')) ?>
     <?php endif; ?>
 </div>
--- whowentout/views/deal_popup.tpl.php
+++ whowentout/views/deal_popup.tpl.php
@@ -1,38 +1,40 @@
+<?php $have_number = $user->cell_phone_number != null; ?>
 <form class="deal_popup" method="post" action="/deal/confirm">
 
     <input type="hidden" name="event_id" value="<?= $event->id ?>"/>
+
+    <?php if (!browser::is_mobile()): ?>
+
+    <div class="phone_number_field <?= $have_number ? 'have_number' : 'missing_number' ?>">
+        <?php if (!$have_number): ?>
+
+            <h2>
+                Type your number below so we can send you the
+                deal.</h1>
+            </h2>
+
+                <input type="text" class="cell_phone_number" name="user[cell_phone_number]"
+                       value="<?= $user->cell_phone_number ?>" autocomplete="off"/>
+
+            <?php else: ?>
+
+                <h2>The deal will be sent to</h2>
+
+                <input type="text" class="cell_phone_number inline" name="user[cell_phone_number]"
+                       value="<?= $user->cell_phone_number ?>" autocomplete="off"/>
+
+                <a href="#edit" class="edit_cell_phone_number">change</a>
+
+            <?php endif; ?>
+
+        <?php endif; ?>
+    </div>
 
     <?= r::deal_preview(array('user' => $user, 'event' => $event, 'orientation' => 'portrait')) ?>
     <?= r::deal_preview(array('user' => $user, 'event' => $event, 'orientation' => 'landscape')) ?>
 
     <?php if (!browser::is_mobile()): ?>
-
-        <?php if ($user->cell_phone_number == null): ?>
-
-        <label>
-            Type your number below so we can send you the
-            deal on <?= $event->date->format('l') ?> night.</h1>
-        </label>
-
-        <div class="phone_number_field">
-            <input type="text" class="cell_phone_number" name="user[cell_phone_number]"
-                   value="<?= $user->cell_phone_number ?>"/>
-        </div>
-
-        <?php else: ?>
-
-            <label>The deal will be sent to</label>
-
-            <div class="phone_number_field">
-                <input type="text" class="cell_phone_number inline" name="user[cell_phone_number]"
-                       value="<?= $user->cell_phone_number ?>"/>
-                <a href="#edit" class="edit_cell_phone_number">change</a>
-            </div>
-
-        <?php endif; ?>
-
-    <p>(You can also access this deal by going to whowentout.com on your phone.)</p>
-
+        <p>(You can also access this deal by going to whowentout.com on your phone.)</p>
     <?php endif; ?>
 
     <input type="submit" value="Continue"/>
