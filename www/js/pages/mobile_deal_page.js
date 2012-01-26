71e734488ca423dcd439af88c97aa33fd0c69be0
Venkat Dinavahi <vendiddy@gmail.com>
2012-01-26 13:56:02 -0500

    hide addr bar attempt for deal attempt

--- www/js/pages/mobile_deal_page.js
+++ www/js/pages/mobile_deal_page.js
@@ -45,20 +45,7 @@ $('.ticket').entwine({
     }
 });
 
-
-
-function hideAddressBar()
-{
-  if(!window.location.hash)
-  {
-      if(document.height < window.outerHeight)
-      {
-          document.body.style.height = (window.outerHeight + 50) + 'px';
-      }
-
-      setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
-  }
-}
-window.addEventListener("load", function(){ if(!window.pageYOffset){ hideAddressBar(); } } );
-window.addEventListener("orientationchange", hideAddressBar );
+/mobile/i.test(navigator.userAgent) && !location.hash && setTimeout(function() {
+  window.scrollTo(0, 1);
+}, 1000);​
 
