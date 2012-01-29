fd03c922e0da29a47197557253ff7b677fb34ef7
Venkat Dinavahi <vendiddy@gmail.com>
2012-01-29 16:01:47 -0500

    removed benchmarking code

--- whowentout/views/event/event_invite.tpl.php
+++ whowentout/views/event/event_invite.tpl.php
@@ -13,27 +13,15 @@
     <ul>
         <?php foreach ($friends as $friend): ?>
             <li>
-                <?php benchmark::start('item'); ?>
                 <label>
-                    <?php benchmark::start('user_thumb'); ?>
                     <?= r::user_thumb(array('user' => $friend)) ?>
-                    <?php benchmark::end('user_thumb'); ?>
 
                     <?php
-                    benchmark::start('invite_is_sent');
                     $invite_is_sent = $invite_engine->invite_is_sent($event, $current_user, $friend);
-                    benchmark::end('invite_is_sent');
-
-                    benchmark::start('is_invited');
                     $is_invited = $invite_engine->is_invited($event, $friend);
-                    benchmark::end('is_invited');
-
-                    benchmark::start('user_has_checked_in');
                     $user_has_checked_in = $checkin_engine->user_has_checked_into_event($friend, $event);
-                    benchmark::end('user_has_checked_in');
                     ?>
 
-                    <?php benchmark::start('checkboxes'); ?>
                     <?php if ($invite_is_sent): //invited by you ?>
                         <input type="checkbox" name="recipients[]" value="<?= $friend->id ?>" checked="checked" disabled="disabled" />
                     <?php elseif ($is_invited): //invited by someone else ?>
@@ -43,7 +31,6 @@
                     <?php else: ?>
                         <input type="checkbox" name="recipients[]" value="<?= $friend->id ?>" />
                     <?php endif; ?>
-                    <?php benchmark::end('checkboxes'); ?>
 
                     <div class="user_first_name">
                         <?= $friend->first_name ?>
@@ -54,7 +41,6 @@
                     </div>
 
                 </label>
-                <?php benchmark::end('item'); ?>
             </li>
         <?php endforeach; ?>
     </ul>
@@ -64,7 +50,5 @@
         <input type="submit" class="send_invites_button" name="send" value="Send Invites" />
         <?= a(app()->event_link($event), 'Skip', array('class' => 'cancel_link')) ?>
     </fieldset>
-
-    <?php krumo::dump(benchmark::summary()); ?>
 
 </form>
