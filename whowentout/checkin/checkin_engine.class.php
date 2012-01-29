b080dd476e3288e5d8836eea148ed6fc128d08ac
Venkat Dinavahi <vendiddy@gmail.com>
2012-01-29 15:54:53 -0500

    cache user_has_checked_in result

--- whowentout/checkin/checkin_engine.class.php
+++ whowentout/checkin/checkin_engine.class.php
@@ -26,6 +26,21 @@ class CheckinEngine
         $this->checkins = $this->database->table('checkins');
     }
 
+    private $event_checkins = array();
+    private function load_event_cache_if_missing($event)
+    {
+        if (!isset($this->event_checkins[$event->id]))
+            $this->load_event_cache($event);
+    }
+    private function load_event_cache($event)
+    {
+        $this->event_checkins[$event->id] = $this->checkins->where('event_id', $event->id)->to_array();
+    }
+    private function clear_event_cache($event)
+    {
+        unset($this->event_checkins[$event->id]);
+    }
+
     function checkin_user_to_event($user, $event)
     {
         $previous_checkin = $this->get_checkin_on_date($user, $event->date);
@@ -42,13 +57,18 @@ class CheckinEngine
                                          'user_id' => $user->id,
                                          'event_id' => $event->id,
                                     ));
+
+        $this->clear_event_cache($event);
     }
 
     function user_has_checked_into_event($user, $event)
     {
-        return $this->checkins->where('user_id', $user->id)
-                       ->where('event_id', $event->id)
-                       ->count() > 0;
+        $this->load_event_cache_if_missing($event);
+        foreach ($this->event_checkins[$event->id] as $checkin)
+            if ($checkin->user_id == $user->id)
+                return true;
+
+        return false;
     }
 
     function get_checkin_on_date($user, DateTime $date)
@@ -66,6 +86,8 @@ class CheckinEngine
         $checkin = $this->get_checkin_on_date($user, $date);
         if ($checkin)
             $this->checkins->destroy_row($checkin->id);
+
+        $this->clear_event_cache($checkin->event);
     }
 
     function get_checkins_for_event($event)
