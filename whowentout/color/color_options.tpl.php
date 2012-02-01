3b556748acbb824f5988517945653f51a47bf01f
Venkat Dinavahi <vendiddy@gmail.com>
2012-02-01 14:59:16 -0500

    moved feature code into a feature folder

--- whowentout/color/color_options.tpl.php
+++
@@ -1,8 +0,0 @@
-<form class="color_options">
-    <label>Main Color</label>
-    <input type="text" name="primary_color" autocomplete="off" value="" />
-
-    <div>
-        <input type="submit" value="Update"/>
-    </div>
-</form>
