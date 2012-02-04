a0f761b0f6f42e58db8dad559f7ab012c9bb147c
Venkat Dinavahi <vendiddy@gmail.com>
2012-02-04 16:47:53 -0500

    tip with number of pending requests is now reallll

--- whowentout/features/entourage/templates/entourage_tab.tpl.php
+++ whowentout/features/entourage/templates/entourage_tab.tpl.php
@@ -1,21 +1,28 @@
 <?php
 /* @var $entourage_engine EntourageEngine */
 $entourage_engine = build('entourage_engine');
-$n = $entourage_engine->get_entourage_count(auth()->current_user());
+$user = auth()->current_user();
+
+$n = $entourage_engine->get_entourage_count($user);
+$requests = $entourage_engine->get_pending_requests($user);
+$num_requests = count($requests);
 ?>
 
 <?php if (auth()->logged_in()): ?>
     <?= a_open('entourage', array('class' => 'entourage_link')); ?>
+
         <?= "My Entourage ($n)" ?>
 
-        <div class="tab_tip_wrapper">
-            <div class="tab_tip">
-                <div class="tip_arrow"></div>
-                <div class="tip_content">
-                    2 new requests
+        <?php if ($num_requests > 0): ?>
+            <div class="tab_tip_wrapper">
+                <div class="tab_tip">
+                    <div class="tip_arrow"></div>
+                    <div class="tip_content">
+                       <?= Inflect::pluralize_if($num_requests, 'new request') ?>
+                    </div>
                 </div>
             </div>
-        </div>
+        <?php endif; ?>
 
     <?= a_close(); ?>
 <?php endif; ?>
