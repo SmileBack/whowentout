41a7a707b77259fe608ddb44f7d02038e3010202
Venkat Dinavahi <vendiddy@gmail.com>
2012-02-04 14:43:20 -0500

    prevent users from viewing events without authorization

--- whowentout/views/event/event_day.display.php
+++ whowentout/views/event/event_day.display.php
@@ -5,6 +5,9 @@ class Event_Day_Display extends Display
 
     function process()
     {
+        if (!auth()->logged_in())
+            show_404();
+
         $this->checkin = $this->checkin_engine->get_checkin_on_date($this->current_user, $this->date);
         $this->checkin_event = $this->checkin ? $this->checkin->event : null;
     }
