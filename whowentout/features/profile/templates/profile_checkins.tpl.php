351efa2a21a73b735a9ef42f842f96055272af01
Venkat Dinavahi <vendiddy@gmail.com>
2012-02-06 13:17:08 -0500

    whole checkin box links to day of event

--- whowentout/features/profile/templates/profile_checkins.tpl.php
+++ whowentout/features/profile/templates/profile_checkins.tpl.php
@@ -4,15 +4,15 @@ $checkin_engine = build('checkin_engine');
 $checkins = $checkin_engine->get_checkins_for_user($user);
 ?>
 <?php if (count($checkins) > 0): ?>
-<ul class="profile_checkins">
+<div class="profile_checkins">
     <?php foreach ($checkins as $checkin): ?>
         <?php $link = app()->event_link($checkin->event); ?>
-        <li>
+        <?= a_open($link, array('class' => 'checkin')); ?>
             <?= r::date(array('date' => $checkin->event->date)); ?>
-            <span class="event"><?= a($link, $checkin->event->name) ?></span>
-        </li>
+            <span class="event"><?= $checkin->event->name ?></span>
+        <?= a_close(); ?>
     <?php endforeach; ?>
-</ul>
+</div>
 <?php else: ?>
     <h2><?= $user->first_name ?> hasn't checked into any parties.</h2>
 <?php endif; ?>
