63823bad5ee5d9479b80e26e7a058bb7cce6325c
Venkat Dinavahi <vendiddy@gmail.com>
2012-02-08 12:17:32 -0500

    shortened add event input for desktop

--- whowentout/views/event/event_add_form.tpl.php
+++ whowentout/views/event/event_add_form.tpl.php
@@ -5,16 +5,10 @@
            name="event_id"
            value="new" />
 
-    <?php
-    $title = 'Going somewhere else?';
-    if (browser::is_desktop())
-        $title .= ' Type place here';
-    ?>
-
     <div class="place">
         <input type="hidden" name="event[date]" value="<?= $date->format('Y-m-d') ?>" />
         <input type="text" class="inline_label" name="event[name]" value="" autocomplete="off"
-               title="<?= $title ?>" />
+               title="Going somewhere else?" />
     </div>
 
     <div class="badge">
