cc9042e484f70b0cd66349915a348aeb44b87088
Venkat Dinavahi <vendiddy@gmail.com>
2012-02-09 09:40:51 -0500

    added installer to test.php

--- www/test.php
+++ www/test.php
@@ -5,3 +5,8 @@ ini_set('display_errors', '1');
 
 require_once '../fire/core/boot.php';
 boot();
+
+/* @var $installer PackageInstaller */
+$installer = build('package_installer');
+$installer->install('WhoWentOutPackage');
+
