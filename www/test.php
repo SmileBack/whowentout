8d9091c0ae679c6cc38830e765a7a981a3e4b192
Venkat Dinavahi <vendiddy@gmail.com>
2012-02-09 09:31:42 -0500

    mysql connect test

--- www/test.php
+++ www/test.php
@@ -5,3 +5,12 @@ ini_set('display_errors', '1');
 
 require_once '../fire/core/boot.php';
 boot();
+
+mysql_connect(
+  $server = "mysql-shared-02.phpfog.com",
+  $username = "Custom App-27847",
+  $password = 'MySQL4668');
+$resut = mysql_select_db("whowentout_com");
+
+var_dump($result);
+
