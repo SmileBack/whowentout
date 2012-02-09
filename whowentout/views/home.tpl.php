8c62bab5a75b17d09332b367383557b042ebdcb2
Venkat Dinavahi <vendiddy@gmail.com>
2012-02-09 18:27:29 -0500

    get your deal

--- whowentout/views/home.tpl.php
+++ whowentout/views/home.tpl.php
@@ -39,7 +39,7 @@
 <body id="home_page">
 
     <div class="home_center">
-        <img src="/images/home.png?version=10" />
+        <img src="/images/home.png?version=11" />
         <a href="/login" class="login_button">Login with Facebook</a>
     </div>
 
