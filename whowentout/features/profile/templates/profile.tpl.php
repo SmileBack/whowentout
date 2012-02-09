598c3de86ecab913754948a639c219e9afb6f610
Venkat Dinavahi <vendiddy@gmail.com>
2012-02-09 14:03:05 -0500

    send message hidden on ur own profile

--- whowentout/features/profile/templates/profile.tpl.php
+++ whowentout/features/profile/templates/profile.tpl.php
@@ -19,7 +19,9 @@
                 </ul>
             </div>
 
+            <?php if (!your_profile): ?>
             <a class="send_message_link coming_soon" href="#send_message">Send Message</a>
+            <?php endif; ?>
         </div>
 
         <?= r::entourage_section(array('user' => $user, 'show_invite_link' => $your_profile)); ?>
