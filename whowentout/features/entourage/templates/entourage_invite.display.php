4734f50d15545015ab28d700b7e3ba1e0590e482
Venkat Dinavahi <vendiddy@gmail.com>
2012-02-09 09:27:16 -0500

    fixed allowed networks for entourage invite

--- whowentout/features/entourage/templates/entourage_invite.display.php
+++ whowentout/features/entourage/templates/entourage_invite.display.php
@@ -14,8 +14,7 @@ class Entourage_Invite_Display extends Display
 
     private function get_allowed_networks()
     {
-        $config = build('allowed_networks');
-        return $config['networks'];
+        $config = (array)build('allowed_networks');
     }
 
 }
