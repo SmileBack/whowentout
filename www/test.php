32e77d3aa702fd80ec1f740e0524c3c853e1a67a
Venkat Dinavahi <vendiddy@gmail.com>
2012-02-09 09:38:37 -0500

    removed debug db

--- www/test.php
+++ www/test.php
@@ -5,15 +5,3 @@ ini_set('display_errors', '1');
 
 require_once '../fire/core/boot.php';
 boot();
-
-$result = mysql_connect(
-  $server = "mysql-shared-02.phpfog.com",
-  $username = "Custom App-27847",
-  $password = 'MySQL4668');
-mysql_select_db("whowentout_com");
-
-var_dump($result);
-
-var_dump(environment());
-
-db();
