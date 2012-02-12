1e4b58a10f09e00a559357cdbe2e0e7e2704c156
Venkat Dinavahi <vendiddy@gmail.com>
2012-02-11 20:53:31 -0500

    changed wording for home page

--- whowentout/views/home.tpl.php
+++ whowentout/views/home.tpl.php
@@ -39,7 +39,7 @@
 <body id="home_page">
 
     <div class="home_center">
-        <img src="/images/home.png?version=19" />
+        <img src="/images/home.png?version=20" />
         <a href="/login" class="login_button">Login with Facebook</a>
     </div>
 
