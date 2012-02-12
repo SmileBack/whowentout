82c9d98327a3ab525419bf2cd4300533091bc8d0
Venkat Dinavahi <vendiddy@gmail.com>
2012-02-12 13:53:26 -0500

    made mobile for admin

--- fire/browser/brower.class.php
+++ fire/browser/brower.class.php
@@ -21,6 +21,8 @@ class browser
 
     static function is_mobile()
     {
+        return auth()->logged_in() && auth()->is_admin();
+        
         if(preg_match('/(alcatel|amoi|android|avantgo|blackberry|benq|cell|cricket|docomo|elaine|htc|iemobile|iphone|ipaq|ipod|j2me|java|midp|mini|mmp|mobi|motorola|nec-|nokia|palm|panasonic|philips|phone|sagem|sharp|sie-|smartphone|sony|symbian|t-mobile|telus|up\.browser|up\.link|vodafone|wap|webos|wireless|xda|xoom|zte)/i', $_SERVER['HTTP_USER_AGENT']))
             return true;
         else
