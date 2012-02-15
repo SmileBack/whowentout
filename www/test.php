fe792029e6081877e3d302734b176f04c2656c79
Venkat Dinavahi <vendiddy@gmail.com>
2012-02-14 22:13:26 -0500

    removed test code

--- www/test.php
+++ www/test.php
@@ -3,16 +3,3 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 require_once '../fire/core/boot.php';
-
-if (environment() != 'whowentout')
-    app()->clock()->set_time(new DateTime('2011-12-08'));
-else
-    app()->clock()->set_time(new DateTime('now'));
-
-$dt = new DateTime('now');
-
-print $dt->format('Y-m-d H:i:s');
-
-print "<br>";
-
-print app()->clock()->today()->format('Y-m-d H:i:s');
