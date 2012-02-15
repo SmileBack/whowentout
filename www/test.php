32c798ab297d3af41314f7bf32e6c6706d88c831
Venkat Dinavahi <vendiddy@gmail.com>
2012-02-14 22:10:36 -0500

    test print login url

--- www/test.php
+++ www/test.php
@@ -4,15 +4,8 @@ ini_set('display_errors', '1');
 
 require_once '../fire/core/boot.php';
 
-if (environment() != 'whowentout')
-    app()->clock()->set_time(new DateTime('2011-12-08'));
-else
-    app()->clock()->set_time(new DateTime('now'));
 
-$dt = new DateTime('now');
+/* @var $fb Facebook */
+$fb = build('facebook');
 
-print $dt->format('Y-m-d H:i:s');
-
-print "<br>";
-
-print app()->clock()->today()->format('Y-m-d H:i:s');
+print auth()->get_login_url();
