ec8325a3679d6fff281427b31a125e248a3cfc27
Venkat Dinavahi <vendiddy@gmail.com>
2012-02-22 12:34:34 -0500

    bar tab competition css

---
+++ whowentout/features/contest/displays/contest_link.tpl.php
@@ -0,0 +1,6 @@
+<h2 class="contest_link">
+
+    <?= a('leaderboard/' . $date->format('Y/m/d'), '$50 bar tab competition', array('class' => 'view_leaderboard')) ?>
+    <p>Current Leader: Joey Schmoey</p>
+
+</h2>
--- whowentout/views/event/event_day.tpl.php
+++ whowentout/views/event/event_day.tpl.php
@@ -1,14 +1,12 @@
 <div class="event_day" data-date="<?= $date->format('Ymd') ?>">
 
-    <?php if (InviteLeaderboard::is_contest_date($date)): ?>
-    <h2 style="text-align: center;">
-    <?= a('leaderboard/' . $date->format('Y/m/d'), '$50 bar tab competition', array('class' => 'view_leaderboard')) ?>
-    </h2>
-    <?php endif; ?>
-
     <?php benchmark::start('event_list'); ?>
     <?= r::event_list(array('date' => $date, 'selected_event' => $checkin_event, 'current_user' => $current_user)) ?>
     <?php benchmark::end('event_list'); ?>
+
+    <?php if (InviteLeaderboard::is_contest_date($date)): ?>
+        <?= r::contest_link(array('date' => $date)); ?>
+    <?php endif; ?>
 
     <?php benchmark::start('event_gallery'); ?>
     <?=
