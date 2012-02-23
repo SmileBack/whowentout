bd3613a60ca2122f65e78a13beb70d3eec3b4dec
Venkat Dinavahi <vendiddy@gmail.com>
2012-02-23 16:41:52 -0500

    competition fix

--- whowentout/features/contest/actions/view_leaderboard.php
+++ whowentout/features/contest/actions/view_leaderboard.php
@@ -5,7 +5,7 @@ class ViewLeaderboardAction extends Action
 
     function execute($y, $m, $d)
     {
-        $date = DateTime::createFromFormat('Y/m/d', "$y/$m/$d");
+        $date = new XDateTime("$y/$m/$d", build('timezone'));
         print r::invite_leaderboard(array(
             'date' => $date,
         ));
