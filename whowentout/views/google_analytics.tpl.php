e586f894a968bc8f2fabd1ef34668e6377251a65
Venkat Dinavahi <vendiddy@gmail.com>
2012-02-23 17:06:51 -0500

    google analytics tracks facebook id and gender

--- whowentout/views/google_analytics.tpl.php
+++ whowentout/views/google_analytics.tpl.php
@@ -1,7 +1,26 @@
+<?php
+if (auth()->logged_in()) {
+    $user = auth()->current_user();
+    $commands = array(
+        array('_setCustomVar', 1, 'facebook_id', $user->facebook_id, 2),
+        array('_setCustomVar', 2, 'gender', $user->gender, 2),
+    );
+}
+else {
+    $commands = array();
+}
+?>
+
 <script type="text/javascript">
+  var commands = <?= json_encode($commands) ?>;
 
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-26468050-1']);
+
+  for (var i = 0; i < commands.length; i++) {
+      _gaq.push(commands[i]);
+  }
+
   _gaq.push(['_trackPageview']);
 
   (function() {
