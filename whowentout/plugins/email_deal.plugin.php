2a16efcc2c70d5e4639b96c58fd2358eaed6448f
Venkat Dinavahi <vendiddy@gmail.com>
2012-03-01 00:36:43 -0500

    changed design of home page

--- whowentout/plugins/email_deal.plugin.php
+++ whowentout/plugins/email_deal.plugin.php
@@ -3,6 +3,9 @@
 class EmailDealPlugin extends Plugin
 {
 
+    /**
+     * @var JobQueue
+     */
     private $queue;
 
     function on_checkin($e)
@@ -10,5 +13,11 @@ class EmailDealPlugin extends Plugin
         $checkin = $e->checkin;
 
         $this->queue = build('job_queue');
+
+        $job = new EmailDealJob(array('checkin_id' => $checkin->id));
+        $this->queue->add($job);
+
+        $this->queue->run_in_background($job->id);
     }
+
 }
--- whowentout/views/home.tpl.php
+++ whowentout/views/home.tpl.php
@@ -38,19 +38,23 @@
 
 <body id="home_page">
 
+    <?php if (browser::is_desktop()): ?>
+        <img src="/images/front_page_logo.png?version=27" class="front_page_logo" />
+    <?php endif; ?>
+
     <div class="home_center">
 
         <?php if (browser::is_desktop()): ?>
-        <img src="/images/home.png?version=22" />
+            <img src="/images/home_explanation.png?version=27" />
         <?php endif; ?>
 
-
         <?php if (browser::is_mobile()): ?>
-        <img src="/images/home_mobile_portrait.png?version=22" class="portrait" />
-        <img src="/images/home_mobile_landscape.png?version=22" class="landscape" />
+            <img src="/images/home_mobile_portrait.png?version=22" class="portrait" />
+            <img src="/images/home_mobile_landscape.png?version=22" class="landscape" />
         <?php endif; ?>
 
         <a href="/login" class="login_button">Login with Facebook</a>
+
     </div>
 
     <?= js() ?>
