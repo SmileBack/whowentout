9fcc5dda8484b1b7c0d4f5004388aa9497f6f097
Venkat Dinavahi <vendiddy@gmail.com>
2012-03-01 15:28:50 -0500

    deal checkin message

--- whowentout/features/deal/templates/deal_popup.tpl.php
+++ whowentout/features/deal/templates/deal_popup.tpl.php
@@ -4,11 +4,16 @@
     <input type="hidden" name="event_id" value="<?= $event->id ?>"/>
 
     <h1>Your deal has been emailed to <?= $user->email ?>.</h1>
-    <h3>You can also get to your deal from your phone's web browser.</h3>
 
     <?php if (browser::is_mobile()): ?>
         <?= r::deal_preview(array('user' => $user, 'event' => $event, 'orientation' => 'portrait')) ?>
         <?= r::deal_preview(array('user' => $user, 'event' => $event, 'orientation' => 'landscape')) ?>
+    <?php else: ?>
+        <?= r::deal_preview(array('user' => $user, 'event' => $event, 'orientation' => 'landscape')) ?>
+    <?php endif; ?>
+
+    <?php if (browser::is_desktop()): ?>
+        <h3>You can also get to your deal from your phone's web browser.</h3>
     <?php endif; ?>
 
     <div class="buttons">
