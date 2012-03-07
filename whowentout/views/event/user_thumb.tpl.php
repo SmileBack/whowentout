25e8c5aeae2ea4bb08c3afc664518e03f4df0640
Venkat Dinavahi <vendiddy@gmail.com>
2012-03-07 18:44:28 -0500

    added mobile invite dialog

--- whowentout/views/event/user_thumb.tpl.php
+++ whowentout/views/event/user_thumb.tpl.php
@@ -1,5 +1,6 @@
 <?php
-$image_url = 'http://graph.facebook.com/' . $user->facebook_id . '/picture?type=normal';
+$type = browser::is_mobile() ? 'square' : 'normal';
+$image_url = 'http://graph.facebook.com/' . $user->facebook_id . '/picture?type=' . $type;
 ?>
 <div class="user_thumb">
     <?= html_element('img', array('src' => $image_url, 'alt' => $user->first_name)) ?>
