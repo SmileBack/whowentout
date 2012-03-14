bfef6ab1a7b6722e29f6fe52fbe672ed946a412b
Dan Berenholtz <dan@whowentout.com>
2012-03-14 14:27:19 -0400

    transferred profile changes onto branch

--- whowentout/features/profile/templates/profile.tpl.php
+++ whowentout/features/profile/templates/profile.tpl.php
@@ -2,37 +2,49 @@
     <div class="profile_inner">
 
         <div class="profile_main">
-            <div class="profile_pic">
-                <?= img($profile_picture_url) ?>
+            
+			<h2 class="profile_name"><?= $user->first_name . ' ' . $user->last_name ?></h2>
+			
+			<div class="profile_top">
+				<div class="profile_pic">
+	                <?= img($profile_picture_url) ?>
 
-                <?php if ($your_profile && browser::is_desktop()): ?>
-                <a title="Edit Profile" href="/profile/picture/edit" class="action profile_edit_picture_link">Edit Pic</a>
-                <?php endif; ?>
-            </div>
+	                <?php if ($your_profile && browser::is_desktop()): ?>
+	                <a title="Edit Profile" href="/profile/picture/edit" class="action profile_edit_picture_link">Edit Pic</a>
+	                <?php endif; ?>
+	            </div>
 
-            <div class="profile_info">
-                <h2><?= $user->first_name . ' ' . $user->last_name ?></h2>
-                <dl>
-                    <dt>Colleges</dt>
-                    <dd>
-                        <ul class="profile_networks">
-                            <?php foreach ($user->networks->where('type', 'college') as $network): ?>
-                            <li><?= $network->name ?></li>
-                            <?php endforeach; ?>
-                        </ul>
-                    </dd>
+	            <div class="profile_info"> 
+	                <dl>
+	                    <dt>Colleges</dt>
+	                    <dd>
+	                        <ul class="profile_networks">
+	                            <?php foreach ($user->networks->where('type', 'college') as $network): ?>
+	                            <li><?= $network->name ?></li>
+	                            <?php endforeach; ?>
+	                        </ul>
+	                    </dd>
 
-                    <dt>Hometown</dt>
-                    <dd><?= $user->hometown ?></dd>
-                </dl>
-            </div>
+	                    <dt>Hometown</dt>
+	                    <dd><?= $user->hometown ?></dd>
+	                </dl>
+	            </div>
 
-            <?php if (!$your_profile): ?>
-            <a class="send_message_link coming_soon" href="#send_message">Send Message</a>
-            <?php endif; ?>
+				<?php if (!$your_profile): ?>
+		        <section class="mutual_friends_section">
+		            <h3>Mutual Friends (<?= count($mutual_friends) ?>)</h3>
+		            <?php if (count($mutual_friends) > 0): ?>
+		                <?= r::profile_gallery(array('users' => $mutual_friends, 'preset' => 'facebook.normal', 'show_networks' => true)); ?>
+		            <?php else: ?>
+		                <h2>You and <?= $user->first_name ?> have no friends in common.</h2>
+		            <?php endif; ?>
+		        </section>
+
+	            <?php if (!$your_profile): ?>
+	            <a class="send_message_link coming_soon" href="#send_message">Send Message</a>
+	            <?php endif; ?>
+			</div>
         </div>
-
-        <?= r::entourage_section(array('user' => $user, 'show_invite_link' => $your_profile)); ?>
 
         <section class="checkins_section">
             <h3>
@@ -41,15 +53,8 @@
             <?= r::profile_checkins(array('user' => $user)); ?>
         </section>
 
-        <?php if (!$your_profile): ?>
-        <section class="mutual_friends_section">
-            <h3>Mutual Friends (<?= count($mutual_friends) ?>)</h3>
-            <?php if (count($mutual_friends) > 0): ?>
-                <?= r::profile_gallery(array('users' => $mutual_friends, 'preset' => 'facebook.normal', 'show_networks' => true)); ?>
-            <?php else: ?>
-                <h2>You and <?= $user->first_name ?> have no friends in common.</h2>
-            <?php endif; ?>
-        </section>
+		<?= r::entourage_section(array('user' => $user, 'show_invite_link' => $your_profile)); ?>
+
         <?php endif; ?>
 
     </div>
