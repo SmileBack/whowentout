55665b8e7d74b775ff97874ccbb1a9af5c780630
Venkat Dinavahi <vendiddy@gmail.com>
2012-03-14 23:19:43 -0400

    filters look aight on mobile now

--- whowentout/features/gallery/gallery_filter.tpl.php
+++ whowentout/features/gallery/gallery_filter.tpl.php
@@ -1,12 +1,13 @@
 <form class="gallery_filter">
 
+    <div>
+        <input type="text" name="keywords" class="search inline_label" autocomplete="off" title="Type a name..."/>
+    </div>
+
     <div class="radio_toggle">
         <label class="members"><input type="radio" name="connection" value="members" checked="checked"><span>Members</span></label>
         <label class="friends"><input type="radio" name="connection" value="friend"><span>Friends</span></label>
         <label class="entourage"><input type="radio" name="connection" value="entourage"><span>Entourage</span></label>
     </div>
 
-    <div>
-        <input type="text" name="keywords" class="search inline_label" autocomplete="off" title="Type a name..."/>
-    </div>
 </form>
