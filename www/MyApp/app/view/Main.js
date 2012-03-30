efabd7a39afcb074aede150af6c36665519b894e
Venkat Dinavahi <vendiddy@gmail.com>
2012-03-30 14:13:46 -0400

    made basic tabs

--- www/MyApp/app/view/Main.js
+++ www/MyApp/app/view/Main.js
@@ -7,38 +7,87 @@ Ext.define("MyApp.view.Main", {
         
         items: [
             {
-                title: 'Welcome',
-                iconCls: 'home',
-                
-                styleHtmlContent: true,
+                title: 'Neighborhood',
+                iconCls: 'team',
                 scrollable: true,
 
-                items: {
-                    docked: 'top',
-                    xtype: 'titlebar',
-                    title: 'Welcome to Sencha Touch 2'
-                },
-                
-                html: [
-                    "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
-                    "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
-                    "and refresh to change what's rendered here."
-                ].join("")
-            },
-            {
-                title: 'Get Started',
-                iconCls: 'action',
-                
                 items: [
                     {
                         docked: 'top',
                         xtype: 'titlebar',
-                        title: 'Getting Started'
+                        title: 'My Neighborhood'
                     },
                     {
-                        xtype: 'video',
-                        url: 'http://av.vimeo.com/64284/137/87347327.mp4?token=1330978144_f9b698fea38cd408d52a2393240c896c',
-                        posterUrl: 'http://b.vimeocdn.com/ts/261/062/261062119_640.jpg'
+                        xtype: 'panel',
+                        html: '<h1>my neighborhood</h1>'
+                    }
+                ]
+            },
+            {
+                title: 'Meet',
+                iconCls: 'search',
+                scrollable: true,
+
+                items: [
+                    {
+                        docked: 'top',
+                        xtype: 'titlebar',
+                        title: 'Meet'
+                    },
+                    {
+                        xtype: 'panel',
+                        html: '<h1>meet</h1>'
+                    }
+                ]
+            },
+            {
+                title: 'Checkin',
+                iconCls: 'download',
+                scrollable: true,
+
+                items: [
+                    {
+                        docked: 'top',
+                        xtype: 'titlebar',
+                        title: 'Checkin'
+                    },
+                    {
+                        xtype: 'panel',
+                        html: '<h1>Checkin</h1>'
+                    }
+                ]
+            },
+            {
+                title: 'My Circle',
+                iconCls: 'time',
+                scrollable: true,
+
+                items: [
+                    {
+                        docked: 'top',
+                        xtype: 'titlebar',
+                        title: 'My Circle'
+                    },
+                    {
+                        xtype: 'panel',
+                        html: '<h1>My Circle</h1>'
+                    }
+                ]
+            },
+            {
+                title: 'Profile',
+                iconCls: 'user',
+                scrollable: true,
+
+                items: [
+                    {
+                        docked: 'top',
+                        xtype: 'titlebar',
+                        title: 'My Profile'
+                    },
+                    {
+                        xtype: 'panel',
+                        html: '<h1>My Profile</h1>'
                     }
                 ]
             }
