79e63a542951f81ee6fbbbe356bf4d798f7bc472
Venkat Dinavahi <vendiddy@gmail.com>
2012-04-10 18:57:24 -0400

    made region code more syntactically ruby

--- app/models/region.rb
+++ app/models/region.rb
@@ -12,18 +12,17 @@ class Region < ActiveRecord::Base
     return false if outside_bounding_box?(point)
 
     contains_point = false
-    i = -1
     j = points.size - 1
-    while (i += 1) < points.size
-      a_point_on_polygon = points[i]
+    points.each_index do |i|
+      point_on_polygon = points[i]
       trailing_point_on_polygon = points[j]
-      if point_is_between_the_ys_of_the_line_segment?(point, a_point_on_polygon, trailing_point_on_polygon)
-        if ray_crosses_through_line_segment?(point, a_point_on_polygon, trailing_point_on_polygon)
+      if point_is_between_the_ys_of_the_line_segment?(point, point_on_polygon, trailing_point_on_polygon) \
+         && ray_crosses_through_line_segment?(point, point_on_polygon, trailing_point_on_polygon)
           contains_point = !contains_point
-        end
       end
       j = i
     end
+
     return contains_point
   end
 
