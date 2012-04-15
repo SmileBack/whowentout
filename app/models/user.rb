7d2ee578deeb9d30452b296980e65fcb1e7c188e
Venkat Dinavahi <vendiddy@gmail.com>
2012-04-15 14:54:20 -0400

    began writing spec for users being created from facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,22 @@
 class User < ActiveRecord::Base
+
+  def self.find_by_token(token)
+    api = Koala::Facebook::API.new(token)
+    profile = api.get_object('me')
+    facebook_id = profile['id'].to_i
+
+    user = User.find_by_facebook_id(facebook_id)
+    if user.nil?
+      user = User.new
+
+      user.facebook_id = profile['id'].to_i
+      user.first_name = profile['first_name']
+      user.last_name = profile['last_name']
+      user.email = profile['email']
+      user.save
+    end
+
+    return user
+  end
+
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe User do
+
+  let(:fb_access_token) do
+    "AAACm1V7H288BAEsdcHrcsMGDmjqTKLchLIFHu2Jh0HeZA0XZBvyuyZCvOGEFr9mMp4oiMFr2Yw6Nq1leTf9FasRZBKKtXUMZD"
+  end
+
+  describe "find_by_token" do
+
+    it "should return the proper fields" do
+      user = User.find_by_token(fb_access_token)
+
+      user.facebook_id.should == 776200121
+      user.first_name.should == 'Venkat'
+      user.last_name.should == 'Dinavahi'
+      user.email.should == 'ven@stanford.edu'
+    end
+
+    it "shouldnt return duplicate users" do
+      user_a = User.find_by_token(fb_access_token)
+      user_b = User.find_by_token(fb_access_token)
+
+      user_a.should == user_b
+      user_a.id.should == user_b.id
+    end
+
+  end
+
+end
