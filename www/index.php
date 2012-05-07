57279f2d24d93546845de7d29e24a267645f6aaf
Venkat Dinavahi <vendiddy@gmail.com>
2012-05-07 12:15:22 -0400

    added mailchimp signup form

--- www/index.php
+++ www/index.php
@@ -35,15 +35,12 @@
   <!-- Prompt IE 6 users to install Chrome Frame. Remove this if you support IE 6.
        chromium.org/developers/how-tos/chrome-frame-getting-started -->
   <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
-  <header>
-
-  </header>
 
   <?= img('iphone.png', array('id' => 'iphone')) ?>
 
-  <footer>
-  </footer>
-
+  <div id="right">
+      <?php include('mailchimp_signup_form.php') ?>
+  </div>
 
   <!-- JavaScript at the bottom for fast page loading -->
 
---
+++ www/mailchimp_signup_form.php
@@ -0,0 +1,16 @@
+<!-- Begin MailChimp Signup Form -->
+<link href="http://cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+<form action="http://elementshq.us2.list-manage.com/subscribe/post?u=2e15fa09d17d62ff09e6590c0&amp;id=fb4d30aab1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+	<label for="mce-EMAIL">Sign up for our private beta</label>
+	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+	<input type="submit" value="Sign up" name="subscribe" id="mc-embedded-subscribe" class="button">
+</form>
+</div>
+
+<!--End mc_embed_signup-->
