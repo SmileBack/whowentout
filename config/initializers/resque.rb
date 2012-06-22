68cd9380c28256a084a28dcb485759a068732cb1
Venkat Dinavahi <vendiddy@gmail.com>
2012-06-22 17:50:02 -0400

    before_initialize for stale db connection

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,7 +2,8 @@ unless ENV["REDISTOGO_URL"].nil?
   uri = URI.parse(ENV["REDISTOGO_URL"])
   Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
-  Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
+  Resque.before_fork = Proc.new { ActiveRecord::Base.establish_connection }
 end
+
 
 
