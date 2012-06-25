13a0460e83487911beada6275a0ecb9470bc35de
Venkat Dinavahi <vendiddy@gmail.com>
2012-06-25 18:02:10 -0400

    added authorization to the api

--- lib/api.rb
+++ lib/api.rb
@@ -1,61 +1,61 @@
 require 'grape'
 
 class WWOApi < Grape::API
-  use Rack::Session::Cookie
-
   prefix 'api'
   version 'v1'
+
   format :json
   default_format :json
 
   helpers do
 
-    def session
-      env['rack.session']
-    end
-
     def current_user
-      User.find_by_token(params[:token])
+      @current_user ||= User.find_by_token(params[:token])
     end
 
     def logged_in?
-      User.find_by_token(params[:token]) != nil
+      current_user != nil
+    end
+
+    def authenticate!
+      token = params[:token]
+      error!({:error => 'Must provide a Facebook access token. None provided'}, 401) if token.nil?
+      error!({:error => 'Invalid or expired Facebook access token.'}, 401) if current_user.nil?
     end
 
   end
 
   post 'location' do
-    user = current_user
+    authenticate!
 
     longitude = params[:longitude].to_f
     latitude = params[:latitude].to_f
 
-    user.update_location(longitude: longitude, latitude: latitude)
+    current_user.update_location(longitude: longitude, latitude: latitude)
 
-    {
-        :success => true
-    }
+    {success: true}
   end
 
   get 'location' do
-    user = current_user
+    authenticate!
 
     {
         success: true,
-        longitude: user.longitude,
-        latitude: user.latitude
+        longitude: current_user.longitude,
+        latitude: current_user.latitude
     }
   end
 
   get 'nearby' do
+    authenticate!
+
     response = {
       success: true,
       users: [],
       current_region: nil
     }
 
-    user = current_user
-    nearby_users = user.nearby_users
+    nearby_users = current_user.nearby_users
     unless nearby_users.nil?
       nearby_users.each do |u|
         response[:users] << {
@@ -68,8 +68,8 @@ class WWOApi < Grape::API
       end
     end
 
-    unless user.current_region.nil?
-      response[:current_region] = user.current_region.name
+    unless current_user.current_region.nil?
+      response[:current_region] = current_user.current_region.name
     end
 
     response
@@ -77,41 +77,10 @@ class WWOApi < Grape::API
 
   get 'me' do
     if logged_in?
-      {
-          :user => current_user
-      }
+      {user: current_user}
     else
-      {
-          :user => nil
-      }
+      {user: nil}
     end
-  end
-
-  get 'login' do
-    token = params[:token]
-
-    error!({:error => 'Must provide a Facebook access token. None provided'}, 401) if token.nil?
-
-    user = User.find_by_token(token)
-    error!({:error => 'Invalid or expired Facebook access token.'}, 401) if user.nil?
-
-    session[:user_id] = user.id
-    session[:token] = token
-
-    {
-        :user => user
-    }
-  end
-
-  get 'logout' do
-    session[:user_id] = nil
-    session[:token] = nil
-
-    puts session.inspect
-
-    {
-        :success => true
-    }
   end
 
 end
